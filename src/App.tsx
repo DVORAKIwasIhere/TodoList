cf4814f5de427ee11aed74bd55dfab3a466bb296
DVORAKIwasIhere <aleksey.altaev@mail.ru>
2021-11-01 16:40:42 +0800

    допиливать не стану

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TodoList } from "./components/List";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: #145094;
+    font-family: Roboto;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+
+        <TodoList />
+
+    </>
   );
 }
 
